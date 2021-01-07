62d168e4f9d39b289c04add387b6df7b90d88aa1
D1935827V <dvestal2102@gmail.com>
2021-01-07 10:32:44 -0800

    d

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-##from urllib.request import urlopen
-#url = "https://investor.vanguard.com/search/?query=FLR"
-#page = urlopen(url)
-#html_bytes = page.read()
-#html = html_bytes.decode("utf-8")
-#print(html)
-# #483D8B color
-
 
 from flask import Flask, render_template
 
